c947c9cb774ac775192deac105c7c6cec3a9411d
khuongdo <dohuukhuong@gmail.com>
2015-07-12 00:32:30 +0700

    Add Unit test, Append MSprojectObjects

---
+++ ObjectsLibrary/MSExcel/ExcelObjects.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ObjectsLibrary.MSExcel
+{
+    class ExcelObjects
+    {
+
+    }
+}
